c9d681b3fa248287b23e034fe077766cfbf12565
javaclass <javaclass@15000515cpu.fec.org>
2017-09-26 10:18:58 -0500

    This is my first program which echos a message

---
+++ src/basics1/CRGRFirstProgram.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package basics1;
+
+/**
+ * @author javaclass via CRGR
+ * Date 09.26.2017  Time: 10:11 AM
+ * Echos out a line
+ */
+public class CRGRFirstProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I hope the entire class has a wonderful day!");
+	}
+
+}
