aba576de244b75b1f8e867a3550b4ec94d8236fb
javaclass <javaclass@15000515cpu.fec.org>
2017-09-27 14:59:42 -0500

    Numbers in reverse

---
+++ src/basics1/ReverseNumbers.java
@@ -0,0 +1,11 @@
+package basics1;
+
+public class ReverseNumbers {
+public static void main(String[]args) {
+	int num=10;
+	while (num>0) {
+		System.out.println("Number is " +num);
+		num--;
+	}
+}
+}
