d96502b8c589bcaad58bb5394626adcb7ea52732
javaclass <javaclass@15000515cpu.fec.org>
2017-09-27 15:26:38 -0500

    ForwardLoop

---
+++ src/basics1/ForwardLoop.java
@@ -0,0 +1,11 @@
+package basics1;
+
+public class ForwardLoop {
+public static void main(String[]args) {
+	int num=0;
+	while (num<=10) {
+		System.out.println("Number is " +num);
+		num++;
+	}
+}
+}
