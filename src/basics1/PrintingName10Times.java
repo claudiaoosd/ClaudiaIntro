a79a44267be820d4f6470aa2bb305f617f8cab50
javaclass <javaclass@15000515cpu.fec.org>
2017-09-27 14:42:02 -0500

    Prints name ten times

---
+++ src/basics1/PrintingName10Times.java
@@ -0,0 +1,11 @@
+package basics1;
+
+public class PrintingName10Times {
+	public static void main(String[]args) {
+		int name=1;
+		while(name<=10) {
+			System.out.println("Claudia");
+		name++;
+}
+}
+}
