7ddee7fd24663152c5946a7f3a59fd183230b2f7
javaclass <javaclass@15000515cpu.fec.org>
2017-09-28 11:44:12 -0500

    class updates

---
+++ src/basics1/ForLoop10to1.java
@@ -0,0 +1,12 @@
+package basics1;
+
+public class ForLoop10to1 {
+	public static void main(String[] args) {
+		
+//initialize, check condition, increment
+		for (int i = 10; i> 0; i--) {
+		System.out.println(i); 
+		}
+	}
+
+}
--- src/basics1/MoreCalculations.java
+++ src/basics1/MoreCalculations.java
@@ -2,29 +2,34 @@ package basics1;
 
 public class MoreCalculations {
 	public static void main(String[] args) {
-// define variables		
+// declare variables		
 		double calculateSum;
-		double calculateprod;
-		double calculatediff; 
-		double calculatequo;
-		double calculatequo2;
+		double calculateProd;
+		double calculateDiff; 
+		double calculateQuo;
+		double calculateQuo2;
+// initialize variable
 		double div1 = 33;
 		double div2 = 4;
 // define equations
-		
-		
 		calculateSum=6+13+4;
-		calculateprod=6*13*4;
-		calculatediff=13-4;
-		calculatequo=13.0/4.0;
-		calculatequo2=div1/div2;
+		calculateProd=6*13*4;
+		calculateDiff=13-4;
+		calculateQuo=13.0/4.0;
+		calculateQuo2=div1/div2;
 		
 // print equations		
 		System.out.println("Sum: " + calculateSum);
-		System.out.println("Product: " +calculateprod);
-		System.out.println("Difference: " +calculatediff);
-		System.out.println("Quotient: " +calculatequo);
-		System.out.println("Quotient2: " +calculatequo2);
+		System.out.println("Product: " +calculateProd);
+		System.out.println("Difference: " +calculateDiff);
+		System.out.println("Quotient: " +calculateQuo);
+		System.out.println("Quotient2: " +calculateQuo2);
+//only gives the remainder of two numbers
+		System.out.println("Remainder of " + "33/4: " +33%4);
+// Pre and post incremental operators demo
+		System.out.println("pre incremental Sum: " + calculateSum++);
+		System.out.println("Value of Sum after printing pre incremental Sum: " + calculateSum);
+		System.out.println("post incremental Sum: " + ++calculateSum);
 	}
 
 }
