8d1ff730bf636eca6942456dc54888d0c0276212
javaclass <javaclass@15000515cpu.fec.org>
2017-09-28 11:05:38 -0500

    Add, subtract, multiply and divide

---
+++ src/basics1/MoreCalculations.java
@@ -0,0 +1,30 @@
+package basics1;
+
+public class MoreCalculations {
+	public static void main(String[] args) {
+// define variables		
+		double calculateSum;
+		double calculateprod;
+		double calculatediff; 
+		double calculatequo;
+		double calculatequo2;
+		double div1 = 33;
+		double div2 = 4;
+// define equations
+		
+		
+		calculateSum=6+13+4;
+		calculateprod=6*13*4;
+		calculatediff=13-4;
+		calculatequo=13.0/4.0;
+		calculatequo2=div1/div2;
+		
+// print equations		
+		System.out.println("Sum: " + calculateSum);
+		System.out.println("Product: " +calculateprod);
+		System.out.println("Difference: " +calculatediff);
+		System.out.println("Quotient: " +calculatequo);
+		System.out.println("Quotient2: " +calculatequo2);
+	}
+
+}
