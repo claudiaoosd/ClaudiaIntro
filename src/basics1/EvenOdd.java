0e8ea9bad7d2646ac569698489c5e4664824c674
javaclass <javaclass@15000515cpu.fec.org>
2017-10-02 09:22:35 -0500

    Successful program to determine if number is even or odd

---
+++ src/basics1/EvenOdd.java
@@ -0,0 +1,16 @@
+package basics1;
+
+public class EvenOdd {
+// write a program to check if a number is even or odd
+	
+	public static void main(String[] args) {
+		
+		int evenOdd;
+		evenOdd = 9;
+		if (evenOdd % 2 == 0) 
+			System.out.println(evenOdd + " is even");
+		else
+			System.out.println(evenOdd + " is odd");
+		
+	}
+}
