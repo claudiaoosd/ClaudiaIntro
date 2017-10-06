cc0c36790b9fab48eac48fe44b1d05896e2a6c39
javaclass <javaclass@15000515cpu.fec.org>
2017-10-06 12:03:50 -0500

    First two characters printed out 3 times

---
+++ src/basics1/FirstTwoCharactersTimes3.java
@@ -0,0 +1,13 @@
+package basics1;
+
+public class FirstTwoCharactersTimes3 {
+
+	public static void main(String[] args) {
+		String EvenLengthString = "ExtraBigWord";
+	
+		System.out.println("First two characters times 3: " + EvenLengthString.substring(0,2) + EvenLengthString.substring(0,2) + EvenLengthString.substring(0,2));
+
+
+	}
+
+}
